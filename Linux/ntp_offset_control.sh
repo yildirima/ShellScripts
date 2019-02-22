7246fd5e74c8983c3447f8d9dcb0c5e27d1c3798
yildirima <yildirimabdrhm@gmail.com>
2019-02-22 10:48:25 +0300

    Update ntp_offset_control.sh

--- Linux/ntp_offset_control.sh
+++ Linux/ntp_offset_control.sh
@@ -5,7 +5,7 @@ if [ "$Offset" -lt "-60000" ] || [ "$Offset" -gt 60000 ] || [ -z  "$Offset" ]
 then
         if [ ! -f /unixadmin/controlfile/ntp_offset_critical ]
         then
-        /opt/OV/bin/opcmsg a=Offset o=Offset severity=Critical msg_text="`hostname`  NTP Offset Problem" node=`hostname`
+        /opt/OV/bin/opcmsg a=Offset o=Offset severity=Critical msg_text="`hostname`  NTP Offset Problem!" node=`hostname`
         touch  /unixadmin/controlfile/ntp_offset_critical
         fi
 else
